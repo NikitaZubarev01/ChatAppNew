2b1d3d62607badfca29196b0851037e641741630
NikitaZubarev01 <90709331+NikitaZubarev01@users.noreply.github.com>
2022-05-29 23:09:08 +0700

    Chat rooms

--- components/ChatRoomItem/ChatRoomItem.tsx
+++ components/ChatRoomItem/ChatRoomItem.tsx
@@ -1,29 +1,50 @@
-import React from 'react';
-import { View, Text, Image, Pressable } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, Pressable, ActivityIndicator } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { DataStore, Auth } from 'aws-amplify';
+import { ChatRoomUser, User } from '../../src/models';
 import styles from './styles';
 
 export default function ChatRoomItem({ chatRoom }) {
-    const user = chatRoom.users[1];
+    const [users, setUsers] = useState<User[]>([]); //all users in this chat room
+    const [user, setUser ] = useState<User|null>(null); //display user 
 
     const navigation = useNavigation();
+
+    useEffect(() =>{
+        const fetchUsers = async () => {
+            const fetchedUsers = (await DataStore.query(ChatRoomUser))
+                .filter(chatRoomUser => chatRoomUser.chatRoom.id === chatRoom.id)
+                .map(chatRoomUser => chatRoomUser.user);
+
+                setUsers(fetchedUsers);
+
+                const authUser = await Auth.currentAuthenticatedUser(); 
+                setUser(fetchedUsers.find(user => user.id !== authUser.attributes.sub) || null );
+        };
+        fetchUsers();
+    }, []);
 
     const onPress = () => {
         navigation.navigate('ChatRoom', { id: chatRoom.id });
     }
 
+    if(!user){
+        return<ActivityIndicator />
+    }
+
     return (
         <Pressable onPress={onPress} style={styles.container}>
           <Image source={{ uri: user.imageUri }} style={styles.image} />
-            {chatRoom.newMessages && <View style={styles.badgeContainer}>
+            {!!chatRoom.newMessages && <View style={styles.badgeContainer}>
                 <Text style={styles.badgeText}>{chatRoom.newMessages}</Text>
             </View>}
             <View style={styles.rightContainer}>
                 <View style={styles.row}>
                     <Text style={styles.name}>{user.name}</Text>
-                    <Text style={styles.text}>{chatRoom.lastMessage.createdAt}</Text>
+                    <Text style={styles.text}>{chatRoom.lastMessage?.createdAt}</Text>
                 </View>
-                <Text numberOfLines={1} style={styles.text}> {chatRoom.lastMessage.content} </Text>
+                <Text numberOfLines={1} style={styles.text}> {chatRoom.lastMessage?.content} </Text>
             </View>
       </Pressable>
     );
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{useState, useEffect } from 'react';
 import { 
   Platform, 
   StatusBar, 
@@ -8,18 +8,24 @@ import {
   Text, 
   Pressable 
 } from 'react-native';
-import { Auth } from 'aws-amplify';
-import { useNavigation } from '@react-navigation/native';
+import { Auth, DataStore } from 'aws-amplify';
+import { ChatRoom, ChatRoomUser } from '../src/models';
 
 import ChatRoomItem from '../components/ChatRoomItem';
-import chatRoomsData from '../assets/dummy-data/ChatsRoom';
-
-const chatRoom1 = chatRoomsData[0];
-const chatRoom2 = chatRoomsData[1];
-const chatRoom3 = chatRoomsData[2];
 
 export default function TabOneScreen () {
-  const navigation = useNavigation();
+  const [chatRooms,setChatRooms] = useState<ChatRoom[]>([]);
+
+  useEffect(() => {
+    const fetchChatRooms = async() => {
+      const userData = await Auth.currentAuthenticatedUser();
+      const chatRooms = (await DataStore.query(ChatRoomUser))
+        .filter(chatRoomUser => chatRoomUser.user.id === userData.attributes.sub)
+        .map(chatRoomUser => chatRoomUser.chatRoom);
+      setChatRooms(chatRooms);
+    };
+    fetchChatRooms();
+  },[]);
 
   const logOut = () => {
     Auth.signOut();
@@ -28,7 +34,7 @@ export default function TabOneScreen () {
   return (
     <View style={styles.page}>
       <FlatList 
-        data={chatRoomsData}
+        data={chatRooms}
         renderItem={({ item }) => <ChatRoomItem chatRoom={item}/> }
         showsVerticalScrollIndicator={false}
       />
